2efa6ae5d6ba1d3cb069390fb34b6f08ec3cf93c
David Njuguna <dnjuguna@gmail.com>
2012-11-30 17:28:18 +0300

    integrate

--- merge.php
+++ merge.php
@@ -5,6 +5,6 @@ require_once('includes.php');
 $companies = dbFetch(dbQuery("SELECT `id`, `name` FROM `radioafrica_crmmigration`.`accounts` GROUP BY `name`"));
 
 foreach($companies as $company){
-	dbQuery(sprintf("UPDATE `radioafrica_importer` SET `sugarID` = '%s' WHERE MATCH(`name`) AGAINST('%s') LIMIT 1", $company['id'], $company['name']));
+	dbQuery(sprintf("UPDATE `radioafrica_importer`.`company` SET `sugarID` = '%s' WHERE MATCH(`name`) AGAINST('%s') LIMIT 1", $company['id'], $company['name']));
 	echo "\n".dbAffectedRows();
 }
