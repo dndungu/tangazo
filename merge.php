1c83d52d02d6a4eebf74d1fabc13c5afe618c25f
David Njuguna <dnjuguna@gmail.com>
2012-12-11 13:29:56 +0300

    save

--- merge.php
+++ merge.php
@@ -6,8 +6,6 @@ ignore_user_abort(true);
 
 require_once('includes.php');
 
-dbQuery("UPDATE `radioafrica_crmmigration`.`accounts` SET `synovateCode` = (SELECT `code` FROM `radioafrica_importer`.`company` WHERE LOWER(`name`) = LOWER(`radioafrica_crmmigration`.`accounts`.`name`) LIMIT 1) WHERE `synovateCode` IS NULL;");
-
 $companies = dbFetch(dbQuery("SELECT * FROM `radioafrica_importer`.`company` WHERE `code` NOT IN (SELECT `synovateCode` FROM `radioafrica_crmmigration`.`accounts` GROUP BY `synovateCode`)"));
 
 if(is_null($companies)) die('no companies to update');
@@ -17,7 +15,7 @@ $query[] = "INSERT INTO `radioafrica_crmmigration` (`id`, synovateCode`, `name`,
 $datetime = date();
 
 foreach($companies as $company){
-	$query[] = sprintf("('%s', '%d', '%s', NOW(), NOW())", createGuid(), $company['code'], $company['name']);
+	$query[] = sprintf("('%s', %d, '%s', NOW(), NOW())", createGuid(), $company['code'], $company['name']);
 }
 
 dbQuery(implode(' ', $query));
