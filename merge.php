696b51dc8bba334b9ca196b72ad64285c3c593c7
David Njuguna <dnjuguna@gmail.com>
2012-12-11 12:45:16 +0300

    save

--- merge.php
+++ merge.php
@@ -1,10 +1,23 @@
 <?php
 
+set_time_limit(0);
+
+ignore_user_abort(true);
+
 require_once('includes.php');
 
-$companies = dbFetch(dbQuery("SELECT `id`, `name` FROM `radioafrica_crmmigration`.`accounts` GROUP BY `name`"));
+dbQuery("UPDATE `radioafrica_crmmigration`.`accounts` SET `synovateCode` = (SELECT `code` FROM `radioafrica_importer`.`company` WHERE LOWER(`name`) = LOWER(`radioafrica_crmmigration`.`accounts`.`name`) LIMIT 1) WHERE `synovateCode` IS NULL;");
+
+$companies = dbFetch(dbQuery("SELECT * FROM `radioafrica_importer`.`company` WHERE `code` NOT IN (SELECT `synovateCode` FROM `radioafrica_crmmigration`.`accounts` GROUP BY `synovateCode`)"));
+
+if(is_null($companies)) die('no companies to update');
+
+$query[] = "INSERT INTO `radioafrica_crmmigration` (`id`, synovateCode`, `name`, `date_entered`, `date_modified`) VALUES";
+
+$datetime = date();
 
 foreach($companies as $company){
-	dbQuery(sprintf("UPDATE `radioafrica_importer`.`company` SET `sugarID` = '%s' WHERE `name` LIKE 's%'", $company['id'], $company['name']));
-	echo "\n".dbAffectedRows();
+	$query[] = sprintf("('%s', '%d', '%s', NOW(), NOW())", createGuid(), $company['code'], $company['name']);
 }
+
+dbQuery(implode(' ', $query));
