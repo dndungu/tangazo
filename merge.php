e7225de0133a07fa0155162f5281beac3720b4b3
David Njuguna <dnjuguna@gmail.com>
2013-04-16 15:36:23 +0300

    save

--- merge.php
+++ merge.php
@@ -13,13 +13,11 @@ $accounts = dbFetch(dbQuery("SELECT `name` FROM `accounts` GROUP BY `name` HAVIN
 foreach($accounts as $account){
 	$name = $account['name'];
 	$updateQuery = sprintf("UPDATE `accounts` SET `code` = (SELECT `code` FROM `accounts` WHERE `name` = '%s' AND `code` IS NOT NULL LIMIT 1) WHERE `code` IS NULL AND `name` = '%s' LIMIT 1", $name, $name);
-	$results['update'][] = $updateQuery;
-	//dbQuery($updateQuery);
-	//$results[$name]['update'][] = dbAffectedRows();
+	dbQuery($updateQuery);
+	$results['update'][$name][] = dbAffectedRows();
 	$deleteQuery = sprintf("DELETE FROM `accounts` WHERE `name` = '%s' AND `creationTime` > 0 LIMIT 1", $name);
-	$results['delete'][] = $deleteQuery;
-	//dbQuery($deleteQuery);
-	//$results[$name]['delete'][] = dbAffectedRows();
+	dbQuery($deleteQuery);
+	$results['delete'][$name][] = dbAffectedRows();
 }
 
 print json_encode($results);
