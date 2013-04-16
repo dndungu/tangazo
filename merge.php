87fb421800af8a853e5dd65715605d11947ed2e6
David Njuguna <dnjuguna@gmail.com>
2013-04-16 15:50:20 +0300

    save

--- merge.php
+++ merge.php
@@ -16,12 +16,12 @@ foreach($accounts as $account){
 	
 	$rows = dbFetch(dbQuery(sprintf("SELECT `code` FROM `accounts` WHERE `name` = '%s' AND `code` IS NOT NULL LIMIT 1", $name)));
 	
-	dbQuery(sprintf("UPDATE `accounts` SET `code` = %d WHERE `code` IS NULL AND `name` = '%s' LIMIT 1", $rows[0]['code'], $name, $name));
-	$results['update'][$name][] = dbAffectedRows();
-	
 	dbQuery(sprintf("DELETE FROM `accounts` WHERE `name` = '%s' AND `creationTime` > 0 LIMIT 1", $name));
 	$results['delete'][$name][] = dbAffectedRows();
 	
+	dbQuery(sprintf("UPDATE `accounts` SET `code` = %d WHERE `code` IS NULL AND `name` = '%s' LIMIT 1", $rows[0]['code'], $name, $name));
+	$results['update'][$name][] = dbAffectedRows();
+		
 }
 
 print json_encode($results);
