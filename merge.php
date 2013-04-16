cebd3444d4d15d717f18aa8bde993913c6326e42
David Njuguna <dnjuguna@gmail.com>
2013-04-16 16:10:54 +0300

    save

--- merge.php
+++ merge.php
@@ -16,7 +16,7 @@ foreach($accounts as $account){
 	
 	$rows = dbFetch(dbQuery(sprintf("SELECT `code` FROM `accounts` WHERE `name` = '%s' AND `code` IS NOT NULL LIMIT 1", $name)));
 	
-	dbQuery(sprintf("DELETE FROM `accounts` WHERE `name` = '%s' AND `code` IS NOT NULL", $name));
+	dbQuery(sprintf("DELETE FROM `accounts` WHERE `name` = '%s' AND `creationTime` IS NOT NULL", $name));
 	$results['delete'][$name][] = dbAffectedRows();
 	
 	dbQuery(sprintf("UPDATE `accounts` SET `code` = %d WHERE `code` IS NULL AND `name` = '%s' LIMIT 1", $rows[0]['code'], $name, $name));
