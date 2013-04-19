2365679dbff719da871e7a3b89e4fad4107ddfaa
David Njuguna <dnjuguna@gmail.com>
2013-04-19 19:17:56 +0300

    Adding chronological navigation

--- panel.php
+++ panel.php
@@ -95,7 +95,7 @@
   		<div class="row header">
   			<div class="column four">
   				<a class="navigation" href="panel.php?filter=<?php print $filter?>&id=<?php print $id?>&offset=<?php print ($offset - 1)?>"><img src="images/previous.png"/></a>
-  				<span style="margin:-10px 0 0 0;display:inline-block;"><?php print $title?></span>
+  				<span><?php print $title?><br/>&nbsp;</span>
   				<?php if($offset < 0){?>
   				<a class="navigation" href="panel.php?filter=<?php print $filter?>&id=<?php print $id?>&offset=<?php print ($offset + 1)?>"><img src="images/next.png"/></a>
   				<?php }?>
