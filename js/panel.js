8d9d8ff2d9ef4be5b0d371e953d5c7475ed95cb1
David Njuguna <dnjuguna@gmail.com>
2013-04-24 20:45:52 +0300

    save

--- js/panel.js
+++ js/panel.js
@@ -30,6 +30,7 @@ var panel = {
 		var yearSelector = $('select[name="year"]', document);
 		var defaultYear = parseInt(yearSelector.attr('default'));
 		var currentYear = parseInt(yearSelector.val());
+		console.info((currentYear === defaultYear));
 		var offset = (currentYear === defaultYear) ? (selectedMonth - defaultMonth) : ((defaultMonth - 1) + ((defaultYear - selectedYear - 1) * 12) + (13 - selectedMonth));
 		this.jumpTo(offset);
 	},
