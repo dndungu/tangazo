573274de9dffff2c2312719b830ddd3e33fcb73f
David Njuguna <dnjuguna@gmail.com>
2013-04-24 21:54:24 +0300

    save

--- js/panel.js
+++ js/panel.js
@@ -28,6 +28,8 @@ var panel = {
 		var defaultWeek = parseInt(weekSelector.attr('default'));
 		var selectedWeek = parseInt(weekSelector.val());
 		var offset = (selectedYear === defaultYear) ? (selectedWeek - defaultWeek) : -((defaultWeek - 1) + ((defaultYear - selectedYear - 1) * 52) + (53 - selectedWeek));
+		console.info('default week :' + defaultWeek);
+		console.info('selected week :' + selectedWeek);
 		this.jumpTo(offset);
 	},
 	doJumpToMonth: function(){
