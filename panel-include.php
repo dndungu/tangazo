9dc111ebec16036fa1cda9b866efbca5b4401212
David Njuguna <dnjuguna@gmail.com>
2013-04-24 22:28:20 +0300

    save

--- panel-include.php
+++ panel-include.php
@@ -97,7 +97,7 @@ function totalSort($a, $b){
 	if($b['rowtotal'] == $a['rowtotal']) return 0;
 	return $a['rowtotal'] > $b['rowtotal'] ? -1 : 1;
 }
-print_r($contentRecords);die();
+//print_r($contentRecords);die();
 if(isset($contentRecords)){
 	foreach($contentRecords as $mediaCode => $contentRecord){
 		$total = 0	;
