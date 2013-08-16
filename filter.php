db156cf6152b7a282a5afc291214b6409ddf60ff
David Njuguna <dnjuguna@gmail.com>
2013-08-16 18:29:00 +0300

    deploy

--- filter.php
+++ filter.php
@@ -79,7 +79,7 @@
 					<?php print $company?>
 				</div>
 				<?php foreach($media as $mediaCode => $outlet){?>
-				<div class="column"><?php @print $spending[$companyCode][$mediaCode]?></div>
+				<div class="column"><?php @print number_format($spending[$companyCode][$mediaCode], 0)?></div>
 				<?php }?>
 			</div>
 		<?php }?>
