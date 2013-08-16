9f6014ef23e890e74d81128429cd5ff957c6afb4
David Njuguna <dnjuguna@gmail.com>
2013-08-16 19:26:16 +0300

    deploy

--- filter.php
+++ filter.php
@@ -104,7 +104,11 @@
 			</div>
 			<?php }?>
 		</div>
-	<?php }?>
+	<?php }else{?>
+	<div class="report-content" style="width:100%;height:200px;">
+		I did not find any records matching dates <?php print $from?> to <?php print $to?>
+	</div>
+	<?php ?>
 	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 </body>
 </html>
