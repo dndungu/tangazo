2214644f1c5a0103bda43ab5ece0cf6c46837516
David Njuguna <dnjuguna@gmail.com>
2013-08-16 18:07:57 +0300

    deploy

--- filter.php
+++ filter.php
@@ -12,11 +12,11 @@
 	font-size:.75em;
 }
 .content .row {
-	border-bottom:1px solid #CCC;
+	border-bottom:1px solid #DDD;
 }
 .content .column {
 	width: 90px;
-	border-right:1px solid #CCC;
+	border-right:1px solid #DDD;
 }
 .content .column:first-child {
 	width:180px;
