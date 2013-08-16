74bb1ae25b6a2148fe117801b00bb3f94e15e6fd
David Njuguna <dnjuguna@gmail.com>
2013-08-16 18:16:25 +0300

    deploy

--- filter.php
+++ filter.php
@@ -26,59 +26,57 @@
 <title>Media Spending :: Imports</title>
 </head>
 <body>
-	<div class="container">
-		<div class="header row">
-			<h1 class="column grid10of10">Media Spending</h1>
-			<ul class="column grid10of10">
-				<li><a href="index.php">Upload</a></li>
-				<li><a href="filter.php" class="current">Filter</a></li>
-				<li><a href="spending.php">Spending</a></li>
-				<li><a href="companies.php">Companies</a></li>
-				<li><a href="brands.php">Brands</a></li>
-				<li><a href="media.php">Media</a></li>
-				<li><a href="sections.php">Sections</a></li>
-				<li><a href="subsections.php">Sub Sections</a></li>
-			</ul>
-		</div>
-		<?php
-		require_once('includes.php');
-		$query[] = 'SELECT SUM(`msa_campaign`.`amount`) AS `amount`, `accounts`.`name` AS `company`, `companyCode`, `msa_media`.`name` AS `media`, `mediaCode`';
-		$query[] = 'FROM `msa_campaign`';
-		$query[] = 'JOIN `accounts` ON (`msa_campaign`.`companyCode` = `accounts`.`code`)';
-		$query[] = 'JOIN `msa_media` ON (`msa_campaign`.`mediaCode` = `msa_media`.`code`)';
-		$query[] = 'WHERE `msa_campaign`.`amount` > 0';
-		$query[] = 'GROUP BY `companyCode`, `mediaCode`';
-		$query[] = 'ORDER BY `amount` DESC';
-		$query[] = 'LIMIT 100';
-		$records = dbFetch(dbQuery(implode(' ', $query)));
-		foreach($records as $record){
-			$companies[$record['companyCode']] = $record['company'];
-			$media[$record['mediaCode']] = $record['media'];
-			$spending[$record['companyCode']][$record['mediaCode']] = $record['amount'];
-		}
-		$columns = count($media);
-		?>
-		<div class="content">
-			<div style="display:block;width:<?php print (200 + ($columns * 110))?>px;">
-				<div class="row header">
-					<div class="column">&nbsp;</div>
-					<?php foreach($media as $outlet){?>
-					<div class="column">
-						<?php print $outlet?>
-					</div>
-					<?php }?>
+	<div class="header row">
+		<h1 class="column grid10of10">Media Spending</h1>
+		<ul class="column grid10of10">
+			<li><a href="index.php">Upload</a></li>
+			<li><a href="filter.php" class="current">Filter</a></li>
+			<li><a href="spending.php">Spending</a></li>
+			<li><a href="companies.php">Companies</a></li>
+			<li><a href="brands.php">Brands</a></li>
+			<li><a href="media.php">Media</a></li>
+			<li><a href="sections.php">Sections</a></li>
+			<li><a href="subsections.php">Sub Sections</a></li>
+		</ul>
+	</div>
+	<?php
+	require_once('includes.php');
+	$query[] = 'SELECT SUM(`msa_campaign`.`amount`) AS `amount`, `accounts`.`name` AS `company`, `companyCode`, `msa_media`.`name` AS `media`, `mediaCode`';
+	$query[] = 'FROM `msa_campaign`';
+	$query[] = 'JOIN `accounts` ON (`msa_campaign`.`companyCode` = `accounts`.`code`)';
+	$query[] = 'JOIN `msa_media` ON (`msa_campaign`.`mediaCode` = `msa_media`.`code`)';
+	$query[] = 'WHERE `msa_campaign`.`amount` > 0';
+	$query[] = 'GROUP BY `companyCode`, `mediaCode`';
+	$query[] = 'ORDER BY `amount` DESC';
+	$query[] = 'LIMIT 100';
+	$records = dbFetch(dbQuery(implode(' ', $query)));
+	foreach($records as $record){
+		$companies[$record['companyCode']] = $record['company'];
+		$media[$record['mediaCode']] = $record['media'];
+		$spending[$record['companyCode']][$record['mediaCode']] = $record['amount'];
+	}
+	$columns = count($media);
+	?>
+	<div class="content">
+		<div style="display:block;width:<?php print (200 + ($columns * 110))?>px;">
+			<div class="row header">
+				<div class="column">&nbsp;</div>
+				<?php foreach($media as $outlet){?>
+				<div class="column">
+					<?php print $outlet?>
 				</div>
-				<?php foreach($companies as $companyCode => $company){?>
-					<div class="row">
-						<div class="column">
-							<?php print $company?>
-						</div>
-						<?php foreach($media as $mediaCode => $outlet){?>
-						<div class="column"><?php //print $spending[$companyCode][$mediaCode]?></div>
-						<?php }?>
-					</div>
 				<?php }?>
 			</div>
+			<?php foreach($companies as $companyCode => $company){?>
+				<div class="row">
+					<div class="column">
+						<?php print $company?>
+					</div>
+					<?php foreach($media as $mediaCode => $outlet){?>
+					<div class="column"><?php //print $spending[$companyCode][$mediaCode]?></div>
+					<?php }?>
+				</div>
+			<?php }?>
 		</div>
 	</div>
 	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
