2f46f08145195e14d59b5d7ff72cd6cf5be532f6
David Njuguna <dnjuguna@gmail.com>
2013-08-16 17:42:13 +0300

    deploy

--- filter.php
+++ filter.php
@@ -5,7 +5,7 @@
 <link href="style.css" media="all" rel="stylesheet" type="text/css" />
 <style type="text/css">
 .column {
-	width:90px;
+	width: 90px;
 }
 </style>
 <title>Media Spending :: Imports</title>
@@ -26,44 +26,39 @@
 			</ul>
 		</div>
 		<?php
-			require_once('includes.php');
-			$query[] = 'SELECT SUM(`msa_campaign`.`amount`) AS `amount`, `accounts`.`name` AS `company`, `companyCode`, `msa_media`.`name` AS `media`, `mediaCode`';
-			$query[] = 'FROM `msa_campaign`';
-			$query[] = 'JOIN `accounts` ON (`msa_campaign`.`companyCode` = `accounts`.`code`)';
-			$query[] = 'JOIN `msa_media` ON (`msa_campaign`.`mediaCode` = `msa_media`.`code`)';
-			$query[] = 'GROUP BY `companyCode`, `mediaCode`';
-			$records = dbFetch(dbQuery(implode(' ', $query)));
-			foreach($records as $record){
+		require_once('includes.php');
+		$query[] = 'SELECT SUM(`msa_campaign`.`amount`) AS `amount`, `accounts`.`name` AS `company`, `companyCode`, `msa_media`.`name` AS `media`, `mediaCode`';
+		$query[] = 'FROM `msa_campaign`';
+		$query[] = 'JOIN `accounts` ON (`msa_campaign`.`companyCode` = `accounts`.`code`)';
+		$query[] = 'JOIN `msa_media` ON (`msa_campaign`.`mediaCode` = `msa_media`.`code`)';
+		$query[] = 'GROUP BY `companyCode`, `mediaCode`';
+		$records = dbFetch(dbQuery(implode(' ', $query)));
+		foreach($records as $record){
 				$companies[$record['companyCode']] = $record['company'];
 				$media[$record['mediaCode']] = $record['media'];
 				$spending[$record['companyCode']][$record['mediaCode']] = $record['amount'];
 			}
-			
-		?>
+
+			?>
 		<div class="content">
 			<div class="row">
 				<div class="column"></div>
 				<?php foreach($media as $outlet){?>
-					<div class="column"><?php print $outlet?></div>					
+				<div class="column">
+					<?php print $outlet?>
+				</div>
 				<?php }?>
 			</div>
 			<?php foreach($companies as $company){?>
-				<div class="row">
-					<div class="column">$company</div>
-					<?php foreach($media as $outlet){?>
-						<div class="column">0.00</div>					
-					<?php }?>
-				</div>
+			<div class="row">
+				<div class="column">$company</div>
+				<?php foreach($media as $outlet){?>
+				<div class="column">0.00</div>
+				<?php }?>
+			</div>
 			<?php {?>
-			
 		</div>
 	</div>
-    <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="js/tangazo.js"></script>
-    <script type="text/javascript">
-    	$(document).ready(function(){
-    		tangazo.init();
-        });
-    </script> 	
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 </body>
 </html>
