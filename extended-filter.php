3a4588e5d50116c38cc4c76b0d5150132f95f9f5
David Njuguna <dnjuguna@gmail.com>
2013-08-18 17:15:02 +0300

    deploy

--- extended-filter.php
+++ extended-filter.php
@@ -13,11 +13,11 @@
 	$query[] = sprintf("AND `startDate` BETWEEN '%s' AND '%s'", $from, $to);
 	if(!is_null($companyFilter)){
 		$cleanCompanyFilter = mysqli_real_escape_string($dbConnection, $companyFilter);
-		$query[] = sprintf("AND MATCH (`accounts`.`name`) AGAINST('%s')", $cleanCompanyFilter);
+		$query[] = sprintf("AND (MATCH (`accounts`.`name`) AGAINST('%s') OR `accounts`.`name LIKE '%%s')", $cleanCompanyFilter);
 	}
 	if(!is_null($mediaFilter)){
 		$cleanMediaFilter = mysqli_real_escape_string($dbConnection, $mediaFilter);
-		$query[] = sprintf("AND MATCH (`msa_media`.`name`) AGAINST('%s')", $cleanMediaFilter);
+		$query[] = sprintf("AND (MATCH (`msa_media`.`name`) AGAINST('%s') OR `msa_media`.`name` LIKE '%%s')", $cleanMediaFilter);
 	}
 	$query[] = 'GROUP BY `brandCode`, `mediaCode`';
 	$query[] = 'ORDER BY `amount` DESC';
